84b56f9f50185978754ddc5310e8768abb22591e
micaalvarezm <malvarezmolina@udesa.edu.ar>
2023-09-11 09:37:11 -0300

    api y fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+//import Home from "./components/Home";
 import "./index.css"
 function App() {
   return (
     <div className= "app">
       <Header/>
+      
       <Footer/>
     </div>
   );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,43 +1,53 @@
-import React from "react";
-/* function Home (){
-fetch('https://api.themoviedb.org/3/movie/popular?api_key=cd221d9b379938868090204c71bbef7e&language=en-US&page=1') //poner bien el endpoint
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data);
-    })
-    .catch(function(error){
-        console.log("el error es: " + error)
-    })
-}
-export default Home; */
-
-//FALTA EL OTRO FETCH PARA TRAER OTRA SECTION
-
 import React, {Component} from 'react';
+
 class Home extends Component{
     constructor (props){
         super(props)
         this.state ={
-            datos: '' //ver si datos esta bien asi
+            peliculas: [],
+            series: [], 
         }
     }
     componentDidMount (){
         fetch('https://api.themoviedb.org/3/movie/popular?api_key=cd221d9b379938868090204c71bbef7e&language=en-US&page=1')
-        .then(response => response.json())
-        .then(data => this.setState(
-            {datos: data.results.poster_path} //fijarnos si el data esta bien o si se saca
-        ))
-        .catch( error => console.log(error))
+            .then(response => response.json())
+            .then(data => {
+                let peliculasArray = []
+                for(let i=0;i<5;i++)          //se usa for? o map y filter?
+                    peliculasArray.push(data.results[i])
+            })
+            this.setState({
+                peliculas: peliculasArray
+            })                                //peliculas de donde sale?
+            .catch( error => console.log(error))
+
+
+        fetch('https://api.themoviedb.org/3/tv/popular?api_key=cd221d9b379938868090204c71bbef7e&language=en-US&page=1')//esta bien?
+            .then(response => response.json())
+                .then(data => {
+                    let seriesArray = []
+                    for(let i=0;i<5;i++)          //se usa for? o map y filter?
+                        seriesArray.push(data.results[i])
+                })
+                this.setState({
+                    series: seriesArray
+                }) //peliculas de donde sale?
+                .catch( error => console.log(error))
+            
     }
+    //VER EL RENDER MANIANA
     render(){
+        console.log("me monte")
         return(
-        <div>
-            {this.state.datos === '' ?
-            <h3>Cargando...</h3> :
-            <h3>{this.state.datos}</h3>}
+         <div>
+             {this.state.datos === '' ?
+             <h3>Cargando...</h3> :
+             <h3>{this.state.datos}</h3>}
         </div>
-        )
-    
+        )};
+
 }
+
+export default Home;
+
+
