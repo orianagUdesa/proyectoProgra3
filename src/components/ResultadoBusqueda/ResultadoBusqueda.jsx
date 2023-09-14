104b536b9a3288aee99ca3b4707788ea0abe326c
micaalvarezm <malvarezmolina@udesa.edu.ar>
2023-09-14 13:37:41 -0300

    buscador bien hecho

--- src/components/ResultadoBusqueda/ResultadoBusqueda.jsx
+++ src/components/ResultadoBusqueda/ResultadoBusqueda.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Header from '../Header/Header.jsx';
 import Footer from '../Footer/Footer.jsx';
+import Card from '../Card/Card.jsx';
 
 
 
@@ -10,7 +11,7 @@ class ResultadoBusqueda extends Component {
         this.state = {
             buscador: this.props.match.params.buscador,
             resultado: []
-        }
+        }}
 
         componentDidMount() {
             fetch(`https://api.themoviedb.org/3/search/multi?api_key=cd221d9b379938868090204c71bbef7e&language=en-US&query=${this.props.match.params.buscador}`)
@@ -22,16 +23,17 @@ class ResultadoBusqueda extends Component {
                 })
                 .catch(error => console.log(error))
         }
-        render()
-        return(console.log(resultado)
+       render(){
+        return(//console.log(resultado)
+        <section>
         <Header/>
-        {this.state.busqueda !== "" ?
-                    <p>Resultados de búsqueda para: {this.state.busqueda}</p> :
-                    <p>Cargando...</p>
-                }
+        {this.state.resultado.map((resultado,index) => <Card pelicula={resultado} key={index} serie={resultado}/>)}
         <Footer/>
+        </section>
         )
+        } 
         
-    }}
-
-//export default ResultadoBusqueda;
+        
+        
+    }
+export default ResultadoBusqueda;
